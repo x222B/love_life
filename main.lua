f109f43c0333afece22d2e3e45ab0cdf0a0b2aeb
Dino Tahto <dinotahto.dev@gmail.com>
2021-11-13 13:34:45 +0100

    refactor and comments

--- main.lua
+++ main.lua
@@ -3,17 +3,55 @@ local board = require("board")
 local WINDOW_WIDTH = 1280
 local WINDOW_HEIGHT = 720
 
-local block_size = 40
+local block_size = 20
 local dimY = math.floor(WINDOW_WIDTH / block_size)
 local dimX = math.floor(WINDOW_HEIGHT / block_size)
 
 local generation = 0
-
 local running = false
 local speed = 1
 
 local mouseX = -1
 local mouseY = -1
+
+function love.load()
+    board.init(dimX,dimY)
+    love.graphics.setFont(love.graphics.setNewFont(16))
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true,
+    })
+end
+
+function love.keypressed(key)
+    -- quit
+    if key == 'q' then
+        love.event.quit()
+    end
+    -- pause / unpause
+    if key == 'p' then
+        running = not running
+    end
+    -- reset
+    if key == 'r' then
+        running = false
+        board.init(dimX,dimY)
+        generation = 0
+    end
+    -- speed up
+    if key == 'k' then
+        if speed >= 0.1 then
+            speed = speed * 0.1
+        end
+    end
+    -- speed down
+    if key == 'j' then
+        if speed <= 1 then
+            speed = speed * 10
+        end
+    end
+end
 
 function love.mousepressed(x, y)
     running = false
@@ -22,44 +60,11 @@ function love.mousepressed(x, y)
     board[mouseY][mouseX] = not board[mouseY][mouseX]
 end
 
-function love.load()
-    board.init(dimX,dimY)
-    love.graphics.setFont(love.graphics.setNewFont(16))
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = false,
-    })
-end
-
-function love.keypressed(key)
-    if key == 'q' then
-        love.event.quit()
-    end
-    if key == 'p' then
-        running = not running
-    end
-    if key == 'r' then
-        board.init(dimX,dimY)
-        generation = 0
-    end
-    if key == 'j' then
-        if speed <= 1 then
-            speed = speed * 10
-        end
-    end
-    if key == 'k' then
-        if speed >= 0.01 then
-            speed = speed * 0.1
-        end
-    end
-end
-
 local dttotal = 0
 function love.update(dt)
     dttotal = dttotal+dt
     if running == true then
-        if dttotal > speed then
+        if dttotal >= speed then
             board.iterate(dimX,dimY)
             generation = generation + 1
             dttotal=0
@@ -68,6 +73,7 @@ function love.update(dt)
 end
 
 function love.draw()
+    -- background
     love.graphics.clear(8/255, 8/255, 8/255, 255/255)
 
     love.graphics.printf('Game of Life!', 4, 0, WINDOW_WIDTH, 'left')
